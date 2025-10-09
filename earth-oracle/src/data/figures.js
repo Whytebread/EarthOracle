2029af2b9216e430037c4b39a7d730e6fbb91c16
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-10-09 16:25:15 -0500

    added inital 16 figures to geomanticFigures array

---
+++ earth-oracle/src/data/figures.js
@@ -0,0 +1,22 @@
+const geomanticFigures = [
+  { name: "Via", pattern: [1, 1, 1, 1] },
+  { name: "Populus", pattern: [0, 0, 0, 0] },
+  { name: "Fortuna Major", pattern: [1, 1, 0, 0] },
+  { name: "Fortuna Minor", pattern: [0, 0, 1, 1] },
+  { name: "Cauda Draconis", pattern: [0, 0, 1, 1] },
+  { name: "Puer", pattern: [0, 0, 1, 1] },
+  { name: "Puella", pattern: [0, 0, 1, 1] },
+  { name: "Carcer", pattern: [0, 0, 1, 1] },
+  { name: "Amissio", pattern: [0, 0, 1, 1] },
+  { name: "Laetitia", pattern: [0, 0, 1, 1] },
+  { name: "Caput Draconis", pattern: [0, 0, 1, 1] },
+  { name: "Conjunctio", pattern: [0, 0, 1, 1] },
+  { name: "Acquisitio", pattern: [0, 0, 1, 1] },
+  { name: "Rubeus", pattern: [0, 0, 1, 1] },
+  { name: "Albus", pattern: [0, 0, 1, 1] },
+  { name: "Tristitia", pattern: [0, 0, 1, 1] },
+  { name: "Poplulus", pattern: [0, 0, 1, 1] },
+  
+];
+
+export default geomanticFigures
