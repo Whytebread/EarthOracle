f7aad38b5c14b872c5ee7e308fc168b11a308df9
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-11-03 09:35:21 -0600

    added elements to figures

--- earth-oracle/src/data/figures.js
+++ earth-oracle/src/data/figures.js
@@ -1,19 +1,83 @@
 export const geomanticFigures = [
-  { name: "Via", pattern: [1, 1, 1, 1] },
-  { name: "Populus", pattern: [0, 0, 0, 0] },
-  { name: "Fortuna Major", pattern: [1, 1, 0, 0] },
-  { name: "Fortuna Minor", pattern: [0, 0, 1, 1] },
-  { name: "Cauda Draconis", pattern: [0, 0, 1, 0] },
-  { name: "Puer", pattern: [1, 0, 0, 1] },
-  { name: "Puella", pattern: [1, 0, 1, 0] },
-  { name: "Carcer", pattern: [0, 0, 1, 1] },
-  { name: "Amissio", pattern: [1, 0, 0, 0] },
-  { name: "Laetitia", pattern: [0, 1, 1, 1] },
-  { name: "Caput Draconis", pattern: [1, 0, 1, 1] },
-  { name: "Conjunctio", pattern: [0, 1, 0, 0] },
-  { name: "Acquisitio", pattern: [0, 0, 1, 1] },
-  { name: "Rubeus", pattern: [1, 1, 1, 0] },
-  { name: "Albus", pattern: [0, 1, 1, 0] },
-  { name: "Tristitia", pattern: [1, 0, 0, 1] },
-  
+  {
+    name: "Via",
+    pattern: [1, 1, 1, 1],
+    element: "Water",
+  },
+  {
+    name: "Populus",
+    pattern: [0, 0, 0, 0],
+    element: "Water",
+  },
+  {
+    name: "Fortuna Major",
+    pattern: [1, 1, 0, 0],
+    element: "Earth",
+  },
+  {
+    name: "Fortuna Minor",
+    pattern: [0, 0, 1, 1],
+    element: "Fire",
+  },
+  {
+    name: "Cauda Draconis",
+    pattern: [0, 0, 1, 0],
+    element: "Fire",
+  },
+  {
+    name: "Puer",
+    pattern: [1, 0, 0, 1],
+    element: "Air",
+  },
+  {
+    name: "Puella",
+    pattern: [1, 0, 1, 0],
+    element: "Water",
+  },
+  {
+    name: "Carcer",
+    pattern: [0, 0, 1, 1],
+    element: "Earth",
+  },
+  {
+    name: "Amissio",
+    pattern: [1, 0, 0, 0],
+    element: "Fire",
+  },
+  {
+    name: "Laetitia",
+    pattern: [0, 1, 1, 1],
+    element: "Fire",
+  },
+  {
+    name: "Caput Draconis",
+    pattern: [1, 0, 1, 1],
+    element: "Earth",
+  },
+  {
+    name: "Conjunctio",
+    pattern: [0, 1, 0, 0],
+    element: "Air",
+  },
+  {
+    name: "Acquisitio",
+    pattern: [0, 0, 1, 1],
+    element: "Air",
+  },
+  {
+    name: "Rubeus",
+    pattern: [1, 1, 1, 0],
+    element: "Air",
+  },
+  {
+    name: "Albus",
+    pattern: [0, 1, 1, 0],
+    element: "Water",
+  },
+  {
+    name: "Tristitia",
+    pattern: [1, 0, 0, 1],
+    element: "Earth",
+  },
+
 ];
